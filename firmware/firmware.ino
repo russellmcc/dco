5d8b0bc15c1c8d8494d404555e95982f6cfafab7
russellmcc <russell.mcclellan@gmail.com>
2013-08-28 01:17:36 -0400

    added hysteresis

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,12 +5,17 @@
 uint32_t running_average; //1.15 format
 
 // optional lowpass filter
-const float alphaFloat = 0;
+const float alphaFloat = 0.7;
 const uint32_t alpha = 0xFFFF * alphaFloat; //0.16
 const uint32_t oneMinusAlpha = 0xFFFF * (1 - alphaFloat); //0.16
 
 // we need 12 bits of ADC
 extern "C" int analogRead12(unsigned char pin);
+
+// for prescale hysteresis
+int16_t lastSwapInput = 0xFFFF;
+int16_t lastSwapPrescale = 100;
+const uint16_t swapThreshold = 20;
 
 void EnableDFLL()
 {
@@ -58,24 +63,43 @@ void setup()
   SetupTimers();
   running_average = analogRead12(8) << 3; //1.15
 }
- 
+
+void UpdateTimers(uint16_t input)
+{
+  uint16_t preScale = pgm_read_word(&kHi[input]);
+  uint16_t period = pgm_read_word(&kLo[input]);
+  if(preScale != lastSwapPrescale)
+  {
+    lastSwapInput = input;
+    lastSwapPrescale = preScale;
+  }
+  // wait until all timers are stabilized
+  while ((TCC0.CTRLGSET & (TC0_PERBV_bm | TC0_CCDBV_bm)) &&
+         (TCC1.CTRLGSET & (TC1_PERBV_bm))){}
+  TCC0.PERBUF = period;
+  TCC0.CCDBUF = period >> 2; // for some reason we have to set this every time?*/
+  TCC1.PERBUF = preScale;
+}
+
 void loop()
 {
   uint16_t input = analogRead12(8); //4.12
   running_average = alpha * running_average >> 16;
   running_average += (oneMinusAlpha * (input << 3)) >> 16;
   input = running_average >> 3;
-  
   input &= 0xFFF; // 12 bit.
 
   // A0 is DACB CH1
   if ( (DACB.STATUS & DAC_CH1DRE_bm) )
     DACB_CH1DATA = pgm_read_word(&kAmps[input]) & 0x0FFF;
+  
+  uint16_t diffFromLastSwap = input > lastSwapInput ?
+                              input - lastSwapInput :
+                              lastSwapInput - input;
+  
+  uint16_t newPrescale = pgm_read_word(&kHi[input]);
+  if((newPrescale == lastSwapPrescale) || 
+     (diffFromLastSwap > swapThreshold))
+    UpdateTimers(input);
     
-  // wait until all timers are stabilized
-  while ((TCC0.CTRLGSET & (TC0_PERBV_bm | TC0_CCDBV_bm)) &&
-         (TCC1.CTRLGSET & (TC1_PERBV_bm))){}
-  TCC0.PERBUF = pgm_read_word(&kLo[input]);
-  TCC0.CCDBUF = 10; // for some reason we have to set this every time?*/
-  TCC1.PERBUF = pgm_read_word(&kHi[input]);
 }
